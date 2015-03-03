f420ee47bda5ad28b27359bffae33288c03d739b
Jacques De Liedekerke <jacnut@gmail.com>
2015-03-03 16:53:02 +0100

    average and index evaluations

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -5,4 +5,13 @@ class Evaluation < ActiveRecord::Base
   def self.average_score(evaluation)
     (evaluation.rating_reputation + evaluation.rating_deal + evaluation.rating_pitch + evaluation.rating_competence + evaluation.rating_commitment)/5
   end
+
+    def self.work_again(would_work_again)
+    if would_work_again == true
+      "I'd work again with this investor"
+    else
+      "I wouldn't want to work with this investor in the futur."
+    end
+  end
+
 end
