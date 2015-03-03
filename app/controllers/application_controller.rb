137e2c424f08258982a48d2fe12dfa0f20e0db2d
Fabien Loup <info@norydev.com>
2015-03-03 14:50:25 +0100

    auth, profile in navbar, default profile pic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_founder! || :authenticate_investor!
+
+  before_action :authenticate
+
+  def authenticate
+    authenticate_founder! unless investor_signed_in?
+  end
+
 end
--- app/models/investor.rb
+++ app/models/investor.rb
@@ -8,9 +8,18 @@ class Investor < ActiveRecord::Base
   has_many :evaluations
 
   has_attached_file :profile_pic,
-    styles: { medium: "300x300#", thumb: "100x100#" }
+    styles: { medium: "300x300#", thumb: "100x100#" },
+    :default_url => "default_founder_picture.svg"
 
   validates_attachment_content_type :profile_pic,
     content_type: /\Aimage\/.*\z/
+
+  def image
+    profile_pic.url(:medium)
+  end
+
+  def avatar
+    profile_pic.url(:thumb)
+  end
 
 end
