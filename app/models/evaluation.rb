7901ed1d57bba338a6dbc845afff5615b25a4b78
Jacques De Liedekerke <jacnut@gmail.com>
2015-03-03 19:06:54 +0100

    error

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -6,12 +6,18 @@ class Evaluation < ActiveRecord::Base
     (rating_reputation + rating_deal + rating_pitch + rating_competence + rating_commitment)/5
   end
 
-    def work_again
+  def work_again
     if would_work_again == true
       "I'd work again with this investor"
     else
       "I wouldn't want to work with this investor in the futur."
     end
   end
+
+  def date
+    created_at.strftime("%Y-%m-%d")
+  end
+
+
 
 end
