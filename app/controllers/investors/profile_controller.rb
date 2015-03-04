67b85a613c43fab3b3baa01bf59c7c9a7d6ee070
Fabien Loup <info@norydev.com>
2015-03-04 15:59:16 +0100

    fixed: websites and links

--- app/controllers/investors/profile_controller.rb
+++ app/controllers/investors/profile_controller.rb
@@ -32,6 +32,6 @@ class Investors::ProfileController < ApplicationController
       params[:investor][:facebook].gsub!(/\Ahttps?:\/\//,"")
       params[:investor][:angellist].gsub!(/\Ahttps?:\/\//,"")
       params[:investor][:linkedin].gsub!(/\Ahttps?:\/\//,"")
-      params.require(:investor).permit(:company_name, :physical_address, :phone_number, :facebook, :twitter, :linkedin, :angellist, :skype, :cell_phone, :website, :contact_person, :contact_cell_phone, :sectors_invested_in, :capital_under_managment, :geographical_focus, :preferred_investment_stage, :nb_projects_invested_in, :profile_pic)
+      params.require(:investor).permit(:company_name, :physical_address, :phone_number, :facebook, :twitter, :linkedin, :angellist, :skype, :cell_phone, :website, :contact_person, :contact_email, :contact_cell_phone, :sectors_invested_in, :capital_under_managment, :geographical_focus, :preferred_investment_stage, :nb_projects_invested_in, :profile_pic)
     end
 end
