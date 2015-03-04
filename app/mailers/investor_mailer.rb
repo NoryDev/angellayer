dde273feaad65f2b5ab3d24a78b29b19e7df7620
Fabien Loup <info@norydev.com>
2015-03-04 12:09:16 +0100

    investormailer generation

---
+++ app/mailers/investor_mailer.rb
@@ -0,0 +1,13 @@
+class InvestorMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.investor_mailer.welcome.subject
+  #
+  def welcome
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
