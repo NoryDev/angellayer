96b13b108216a7cb136a1e83be53851bb04754cd
Fabien Loup <info@norydev.com>
2015-03-05 12:26:00 +0100

    new investor possible, routes fixed

--- app/controllers/investors/profile_controller.rb
+++ app/controllers/investors/profile_controller.rb
@@ -1,22 +1,19 @@
 class Investors::ProfileController < ApplicationController
-  before_action :set_investor, only: [:show, :edit, :update]
+  before_action :set_profile, only: [:show, :edit, :update]
 
   def index
-    @investors = Investor.all
+    @profiles = InvestorProfile.all
   end
 
   def show
-    @profile = @investor.investor_profile
   end
 
   def edit
-    @profile = @investor.investor_profile
   end
 
   def update
-    @profile = @investor.investor_profile
-    if @profile.update(investor_params)
-      redirect_to investors_profile_path(@investor), notice: 'Your profile was successfully updated.'
+    if @profile.update(profile_params)
+      redirect_to investors_profile_path(@profile), notice: 'Your profile was successfully updated.'
     else
       render :edit
     end
@@ -27,21 +24,28 @@ class Investors::ProfileController < ApplicationController
   end
 
   def create
+    @profile = InvestorProfile.new(profile_params)
+
+    if @profile.save
+      redirect_to investors_profile_path(@profile), notice: "Profile for #{@profile.company_name} was successfully created."
+    else
+      render :new
+    end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_investor
-      @investor = Investor.find(params[:id])
+    def set_profile
+      @profile = InvestorProfile.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
-    def investor_params
+    def profile_params
       params[:investor_profile][:website].gsub!(/\Ahttps?:\/\//,"")
       params[:investor_profile][:twitter].gsub!(/\Ahttps?:\/\//,"")
       params[:investor_profile][:facebook].gsub!(/\Ahttps?:\/\//,"")
       params[:investor_profile][:angellist].gsub!(/\Ahttps?:\/\//,"")
       params[:investor_profile][:linkedin].gsub!(/\Ahttps?:\/\//,"")
-      params.require(:investor_profile).permit(:company_name, :physical_address, :phone_number, :facebook, :twitter, :linkedin, :angellist, :skype, :cell_phone, :website, :contact_person, :contact_email, :contact_cell_phone, :sectors_invested_in, :capital_under_managment, :geographical_focus, :preferred_investment_stage, :nb_projects_invested_in, :profile_pic)
+      params.require(:investor_profile).permit(:company_name, :email, :physical_address, :phone_number, :facebook, :twitter, :linkedin, :angellist, :skype, :cell_phone, :website, :contact_person, :contact_email, :contact_cell_phone, :sectors_invested_in, :capital_under_managment, :geographical_focus, :preferred_investment_stage, :nb_projects_invested_in, :profile_pic)
     end
 end
