0b7bed86e0c9361c703e11126948567592a30d52
Fabien Loup <info@norydev.com>
2015-03-05 15:59:30 +0100

    working seed -no pics

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,39 +6,75 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Investor.all.each do |investor|
-  investor.destroy
+5.times do
+  founder = Founder.new
+  founder.email = "#{Faker::Internet.user_name}@yopmail.com"
+  founder.password = "12345678"
+  founder.password_confirmation = "12345678"
+  founder.save
+
+  founder.first_name = Faker::Name.first_name
+  founder.last_name = Faker::Name.last_name
+  founder.facebook = Faker::Internet.url('facebook.com')
+  founder.twitter = Faker::Internet.url('twitter.com')
+  founder.linkedin = Faker::Internet.url('linkedin.com')
+  founder.angellist = Faker::Internet.url('angel.co')
+  founder.skype = Faker::Internet.user_name
+  founder.cell_phone = Faker::PhoneNumber.cell_phone
+  founder.website = Faker::Internet.url
+  founder.all_startups = "#{Faker::Company.name}, #{Faker::Company.name} & #{Faker::Company.name}"
+  founder.save
 end
 
-InvestorProfile.all.each do |investor|
-  investor.destroy
-end
-
-10.times do
-  investor = Investor.new()
+3.times do
+  investor = Investor.new
   investor.email = "#{Faker::Internet.user_name}@yopmail.com"
   investor.password = "12345678"
   investor.password_confirmation = "12345678"
   investor.save
+end
 
-  investor = InvestorProfile.new()
-  investor.email = "#{Faker::Internet.user_name}@yopmail.com"
-  investor.company_name = Faker::Company.name
-  investor.physical_address = "#{Faker::Address.street_address}, #{Faker::Address.zip_code} #{Faker::Address.city}"
-  investor.phone_number = Faker::PhoneNumber.phone_number
-  investor.facebook = Faker::Internet.url('facebook.com')
-  investor.twitter = Faker::Internet.url('twitter.com')
-  investor.linkedin = Faker::Internet.url('linkedin.com')
-  investor.angellist = Faker::Internet.url('angel.co')
-  investor.skype = Faker::Internet.user_name
-  investor.website = Faker::Internet.url
-  investor.sectors_invested_in = Faker::Commerce.department
-  investor.capital_under_managment = rand(101) * 100_000
-  investor.geographical_focus = "#{Faker::Address.country}, #{Faker::Address.country} & #{Faker::Address.country}"
-  investor.preferred_investment_stage = ["seed", "stage A", "stage B", "stage C", "any"].sample
-  investor.nb_projects_invested_in = rand(200)
-  investor.contact_person = Faker::Name.name
-  investor.contact_email = "#{Faker::Internet.user_name}@yopmail.com"
-  investor.contact_cell_phone = Faker::PhoneNumber.cell_phone
-  investor.save
+10.times do
+  profile = InvestorProfile.new
+  profile.email = "#{Faker::Internet.user_name}@yopmail.com"
+  profile.company_name = Faker::Company.name
+  profile.physical_address = "#{Faker::Address.street_address}, #{Faker::Address.zip_code} #{Faker::Address.city}"
+  profile.phone_number = Faker::PhoneNumber.phone_number
+  profile.facebook = Faker::Internet.url('facebook.com')
+  profile.twitter = Faker::Internet.url('twitter.com')
+  profile.linkedin = Faker::Internet.url('linkedin.com')
+  profile.angellist = Faker::Internet.url('angel.co')
+  profile.skype = Faker::Internet.user_name
+  profile.website = Faker::Internet.url
+  profile.sectors_invested_in = Faker::Commerce.department
+  profile.capital_under_managment = rand(101) * 100_000
+  profile.geographical_focus = "#{Faker::Address.country}, #{Faker::Address.country} & #{Faker::Address.country}"
+  profile.preferred_investment_stage = ["seed", "stage A", "stage B", "stage C", "any"].sample
+  profile.nb_projects_invested_in = rand(200)
+  profile.contact_person = Faker::Name.name
+  profile.contact_email = "#{Faker::Internet.user_name}@yopmail.com"
+  profile.contact_cell_phone = Faker::PhoneNumber.cell_phone
+  profile.save
+
+  Founder.all.each do |founder|
+    evaluation = Evaluation.new
+    evaluation.founder = founder
+    evaluation.investor_profile = profile
+    evaluation.title_review = Faker::Company.bs
+    evaluation.review = Faker::Lorem.paragraph(20)
+    evaluation.rating_reputation = (0..5).to_a.sample
+    evaluation.rating_deal = (0..5).to_a.sample
+    evaluation.rating_pitch = (0..5).to_a.sample
+    evaluation.rating_competence = (0..5).to_a.sample
+    evaluation.rating_commitment = (0..5).to_a.sample
+    evaluation.amount_raised = 10_000 * rand(100)
+    evaluation.would_work_again = [true, false].sample
+    evaluation.save
+  end
+end
+
+Investor.all.each_with_index do |this_investor, index|
+  i = InvestorProfile.find(index+1)
+  this_investor.investor_profile = i
+  this_investor.save
 end
