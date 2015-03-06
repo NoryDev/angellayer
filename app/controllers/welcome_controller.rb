f52050a5986091e3d55f92a3fcb10cdcfc1e7348
Jacques De Liedekerke <jacnut@gmail.com>
2015-03-06 11:05:24 +0100

    snippets on front-page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate
 
   def index
+    @evaluations = Evaluation.all.order(:created_at)
     render layout: "fullpage"
+
   end
 end
