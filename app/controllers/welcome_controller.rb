2eb29dab347e8e5bc23703b6c1182e3d21588965
Fabien Loup <info@norydev.com>
2015-03-10 14:05:34 +0100

    now with good error messages and refill fields

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,41 +12,36 @@ class WelcomeController < ApplicationController
 
   def sign_up
     params = sign_up_params
-    @sign_email = params[:email]
+    flash[:sign_email] = params[:email]
 
     if params[:is_investor] == "1"
-      @is_investor = true
-      if sign_up_params[:password] == sign_up_params[:password_confirmation]
-        investor = Investor.new(email: sign_up_params[:email], password: sign_up_params[:password], password_confirmation: sign_up_params[:password_confirmation])
-        if investor.save
-          sign_in_and_redirect investor
-        else
-          index
-        end
+      flash[:is_investor] = true
+      investor = Investor.new(email: sign_up_params[:email], password: sign_up_params[:password], password_confirmation: sign_up_params[:password_confirmation])
+      if investor.save
+        sign_in_and_redirect investor
       else
-        index
+        @alert = investor.errors.map{ |key, value| "#{key} #{value}" }.join(", ")
+        redirect_to(root_path, alert: @alert)
       end
     elsif params[:is_investor] == "0"
-      @is_investor = false
-      if sign_up_params[:password] == sign_up_params[:password_confirmation]
-        founder = Founder.new(email: sign_up_params[:email], password: sign_up_params[:password], password_confirmation: sign_up_params[:password_confirmation])
-        if founder.save
-          sign_in_and_redirect founder
-        else
-          index
-        end
+      flash[:is_investor] = false
+      founder = Founder.new(email: sign_up_params[:email], password: sign_up_params[:password], password_confirmation: sign_up_params[:password_confirmation])
+      if founder.save
+        sign_in_and_redirect founder
       else
-        index
+        @alert = founder.errors.map{ |key, value| "#{key} #{value}" }.join(", ")
+        redirect_to(root_path, alert: @alert)
       end
     else
-      index
+      @alert = "You must be either a founder or an investor"
+      redirect_to(root_path, alert: @alert)
     end
 
   end
 
   def log_in
     params = log_in_params
-    @log_email = params[:email]
+    flash[:log_email] = params[:email]
     investor = Investor.find_by_email(params[:email])
     founder = Founder.find_by_email(params[:email])
 
@@ -55,18 +50,18 @@ class WelcomeController < ApplicationController
         sign_in_and_redirect investor
       else
         @alert = "invalid email or password"
-        index
+        redirect_to(root_path, alert: @alert, log_email: @log_email)
       end
     elsif founder
       if founder.valid_password?(params[:password])
         sign_in_and_redirect founder
       else
         @alert = "invalid email or password"
-        index
+        redirect_to(root_path, alert: @alert)
       end
     else
       @alert = "invalid email or password"
-      index
+      redirect_to(root_path, alert: @alert)
     end
   end
 
