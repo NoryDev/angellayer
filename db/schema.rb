654068f53938e141a155fc7f4b2df1860920b184
Fabien Loup <info@norydev.com>
2015-03-10 16:23:41 +0100

    initilization

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310143341) do
+ActiveRecord::Schema.define(version: 20150310150934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
+  enable_extension "fuzzystrmatch"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
