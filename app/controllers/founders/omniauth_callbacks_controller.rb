d977a536f6d8f313fd725e3e3eef4706eeec5af0
Fabien Loup <info@norydev.com>
2015-03-12 12:59:20 +0100

    angellist not working, remove

--- app/controllers/founders/omniauth_callbacks_controller.rb
+++ app/controllers/founders/omniauth_callbacks_controller.rb
@@ -23,18 +23,6 @@ class Founders::OmniauthCallbacksController < Devise::OmniauthCallbacksControlle
     end
   end
 
-  def angellist
-    founder = Founder.find_for_angellist_oauth(request.env["omniauth.auth"])
-
-    if founder.persisted?
-      sign_in_and_redirect founder, event: :authentication
-      set_flash_message(:notice, :success, :kind => "AngelList") if is_navigational_format?
-    else
-      session["devise.angellist_data"] = request.env["omniauth.auth"]
-      redirect_to new_founder_registration_url
-    end
-  end
-
   def linkedin
     auth = env["omniauth.auth"]
     @founder = Founder.connect_to_linkedin(request.env["omniauth.auth"], current_founder)
