637a4520ab8260c5cbd0d441be52b07704a247a4
Fabien Loup <info@norydev.com>
2015-03-22 22:09:50 +0100

    confirmable works in development

--- app/models/founder.rb
+++ app/models/founder.rb
@@ -4,7 +4,7 @@ class Founder < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [ :facebook, :twitter, :linkedin ]
 
