43fb5ac16febbb355298e55a52d7b7724f499356
Jacques De Liedekerke <jacnut@gmail.com>
2015-03-25 17:51:09 +0100

    improved investor dropdown

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -3,9 +3,13 @@ $(document).ready(function () {
   $('#submit-profile').click(function (e) {
     var profile_id = $( "#select-investor" ).val();
     console.log(profile_id)
-
-    document.location.href='/investors/profile/' + profile_id;
+    if(profile_id == 0) {
+      document.location.href='/investors/profile/';
+    }
+    else {
+      document.location.href='/investors/profile/' + profile_id + '/evaluations/new';
     // prevent the default click action
+    }
     e.preventDefault();
   });
 });
