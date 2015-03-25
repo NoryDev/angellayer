e76d0eb0494dabe8d185ed67d17a90e62676eab4
Jacques De Liedekerke <jacnut@gmail.com>
2015-03-25 16:08:25 +0100

    modified dashboard

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,13 +1,11 @@
 // when the page is ready for manipulation
 $(document).ready(function () {
-
   $('#submit-profile').click(function (e) {
-        var profile_id = $( "#select-investor" ).val();
-        console.log(profile_id)
+    var profile_id = $( "#select-investor" ).val();
+    console.log(profile_id)
 
-        document.location.href='/investors/profile/' + profile_id;
-        // prevent the default click action
-        e.preventDefault();
-
+    document.location.href='/investors/profile/' + profile_id;
+    // prevent the default click action
+    e.preventDefault();
   });
 });
