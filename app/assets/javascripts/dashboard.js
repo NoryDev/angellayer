4db91efa606d50f0d1bd10bc4283965c01a8b5ef
Jacques De Liedekerke <jacnut@gmail.com>
2015-03-25 15:53:28 +0100

    added jquery in dropdown

---
+++ app/assets/javascripts/dashboard.js
@@ -0,0 +1,13 @@
+// when the page is ready for manipulation
+$(document).ready(function () {
+
+  $('#submit-profile').click(function (e) {
+        var profile_id = $( "#select-investor" ).val();
+        console.log(profile_id)
+
+        document.location.href='/investors/profile/' + profile_id;
+        // prevent the default click action
+        e.preventDefault();
+
+  });
+});
