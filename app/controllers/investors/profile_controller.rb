bbed750014aeb40ec5e777c2f7d6d02ce216df8d
Fabien Loup <info@norydev.com>
2015-04-09 15:31:17 +0300

    links works

--- app/controllers/investors/profile_controller.rb
+++ app/controllers/investors/profile_controller.rb
@@ -95,11 +95,6 @@ class Investors::ProfileController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def profile_params
-      params[:investor_profile][:website].gsub!(/\Ahttps?:\/\//,"")
-      params[:investor_profile][:twitter].gsub!(/\Ahttps?:\/\//,"")
-      params[:investor_profile][:facebook].gsub!(/\Ahttps?:\/\//,"")
-      params[:investor_profile][:angellist].gsub!(/\Ahttps?:\/\//,"")
-      params[:investor_profile][:linkedin].gsub!(/\Ahttps?:\/\//,"")
       params.require(:investor_profile).permit(:company_name, :email, :physical_address, :phone_number, :facebook, :twitter, :linkedin, :angellist, :skype, :cell_phone, :website, :contact_person, :contact_email, :contact_cell_phone, :sectors_invested_in, :capital_under_managment, :geographical_focus, :preferred_investment_stage, :nb_projects_invested_in, :profile_pic)
     end
 end
