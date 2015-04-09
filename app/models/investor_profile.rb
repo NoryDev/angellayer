031b223528e101d0a3dabb2be131d16e7c474884
Fabien Loup <info@norydev.com>
2015-04-09 15:02:39 +0300

    migration made

--- app/models/investor_profile.rb
+++ app/models/investor_profile.rb
@@ -3,6 +3,8 @@ class InvestorProfile < ActiveRecord::Base
   multisearchable :against => [:company_name, :physical_address, :sectors_invested_in, :geographical_focus, :preferred_investment_stage, :contact_person]
 
   belongs_to :investor
+  belongs_to :author_as_founder, :class_name => "Founder"
+  belongs_to :author_as_investor, :class_name => "Investor"
 
   has_many :evaluations, dependent: :destroy
 
