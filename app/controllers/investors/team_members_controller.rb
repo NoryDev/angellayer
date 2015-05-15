cc284dd04791e52b34d49941389fc80ba214ead2
Fabien Loup <info@norydev.com>
2015-05-15 16:46:38 +0300

    edit ok

--- app/controllers/investors/team_members_controller.rb
+++ app/controllers/investors/team_members_controller.rb
@@ -3,28 +3,15 @@ class Investors::TeamMembersController < ApplicationController
   before_action :find_profile, only: [:new, :edit, :create]
   before_action :set_team_member, only: [:show, :edit, :update]
 
-  skip_before_action :founder_not_authorized, only: [:show, :new]
-  skip_before_action :investor_not_authorized, only: [:show, :new]
+  skip_before_action :founder_not_authorized
+  skip_before_action :investor_not_authorized
+
+  before_action :check_authorisation, only: [:new, :edit]
 
   def show
   end
 
   def new
-
-    if @profile.investor_id
-      if current_investor && (current_investor.investor_profile != @profile)
-        investor_not_authorized
-      end
-    elsif current_founder
-      if current_founder != @profile.author_as_founder
-        founder_not_authorized
-      end
-    elsif current_investor
-      if current_investor != @profile.author_as_investor
-        investor_not_authorized
-      end
-    end
-
     @team_member = TeamMember.new
     @team_member.investor_profile = @profile
   end
@@ -56,9 +43,28 @@ class Investors::TeamMembersController < ApplicationController
       @team_member = TeamMember.find(params[:id])
     end
 
-
     # Only allow a trusted parameter "white list" through.
     def team_member_params
       params.require(:team_member).permit(:first_name, :last_name, :email, :phone, :job_title, :speciality, :year_joined, :facebook, :twitter, :linkedin, :angellist, :skype)
+    end
+
+    def check_authorisation
+
+      if @profile.investor_id
+        if current_investor && (current_investor.investor_profile != @profile)
+          investor_not_authorized
+        end
+      else
+        if current_founder
+          if current_founder != @profile.author_as_founder
+            founder_not_authorized
+          end
+        elsif current_investor
+          if current_investor != @profile.author_as_investor
+            investor_not_authorized
+          end
+        end
+      end
+
     end
 end
